1b08efdb7807133e5e084554e22c3a1e183360d2
Simon Hoyos <shmesa23@gmail.com>
2020-12-17 14:57:19 -0500

    connect to mongo atlas

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connect = require('./db');
+
 
 const port = process.env.PORT || 8000;
 const app = express();
+connect()
 
 app.use(cors());
 app.use(express.json());
