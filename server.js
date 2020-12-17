ade62eec556cca6724470ba48bf92a857db29e46
Simon Hoyos <shmesa23@gmail.com>
2020-12-17 09:03:19 -0500

    Clean code

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 
 const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
